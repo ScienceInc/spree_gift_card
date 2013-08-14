aba237898d758d3c34d963f4278fa5a54d81e54b
Bryce <bcfrye19@gmail.com>
2013-08-14 16:18:01 -0700

    apply_gift_code before OrdersController update to avoid multiple update action issues

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,26 +1,12 @@
 Spree::OrdersController.class_eval do
-
-  # TODO Apply gift code in a before filter if possible to avoid overriding the update method for easier upgrades?  
-  def update
-    @order = current_order
-    if @order.update_attributes(params[:order])
-      render :edit and return unless apply_coupon_code
-      render :edit and return unless apply_gift_code
+  before_filter :apply_gift_card, only: :update
   
-      @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
-      fire_event('spree.order.contents_changed')
-      respond_with(@order) do |format|
-        format.html do
-          if params.has_key?(:checkout)
-            redirect_to checkout_state_path(@order.checkout_steps.first)
-          else
-            redirect_to '/checkout/edit'
-          end
-        end
+  private
+    def apply_gift_card
+      @order = current_order
+      if @order.update_attributes(params[:order])
+        render :edit and return unless apply_gift_code
       end
-    else
-      respond_with(@order)
     end
-  end
 
 end
