2c3c213ff42b406ba4fab2ed0228d32341376901
Bryce <bcfrye19@gmail.com>
2013-08-14 17:09:12 -0700

    generator now supports adding stylesheets to scss files. some formatting fixes

--- lib/generators/spree_gift_card/install_generator.rb
+++ lib/generators/spree_gift_card/install_generator.rb
@@ -3,14 +3,15 @@ module SpreeGiftCard
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "\n//= require store/spree_gift_card\n"
-        append_file "app/assets/javascripts/admin/all.js", "\n//= require admin/spree_gift_card\n"
+        append_file "app/assets/javascripts/store/all.js", "\n//= require store/spree_gift_card"
+        append_file "app/assets/javascripts/admin/all.js", "\n//= require admin/spree_gift_card"
       end
 
       def add_stylesheets
-        # TODO make these work with css and scss
-        inject_into_file "app/assets/stylesheets/store/all.css.scss", " *= require store/spree_gift_card\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_gift_card\n", :before => /\*\//, :verbose => true
+        store_ext = File.exists?("app/assets/stylesheets/store/all.css") ? ".css" : ".css.scss"
+        admin_ext = File.exists?("app/assets/stylesheets/admin/all.css") ? ".css" : ".css.scss"
+        inject_into_file "app/assets/stylesheets/store/all#{store_ext}", "\n *= require store/spree_gift_card", :after => "*= require_tree .", :verbose => true
+        inject_into_file "app/assets/stylesheets/admin/all#{admin_ext}", "\n *= require admin/spree_gift_card", :after => "*= require_tree .", :verbose => true
       end
 
       def add_migrations
