bb3ae834559ec4845ee3d9eede0e86f3914116f1
BryceFrye <bcfrye19@gmail.com>
2013-12-17 11:11:23 -0800

    remove apply gift card override

--- app/overrides/gift_card.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(virtual_path: "spree/checkout/edit",
-                     insert_after: "#checkout-summary",
-                     partial: "spree/overrides/add_gift_code_field",
-                     name: "add_gift_code_field")
