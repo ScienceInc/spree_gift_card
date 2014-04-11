180e4b49e0a67556621ecefc33c78cc9a11dcb3f
BryceFrye <bcfrye19@gmail.com>
2014-04-10 17:18:24 -0700

    fix for prepending a '0' to codes since excel chops them off

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -4,6 +4,7 @@ Spree::StoreController.class_eval do
 
     def apply_gift_code
       return true if @order.gift_code.blank?
+      @order.gift_code = "0" + @order.gift_code if @order.gift_code.length == 15
       if gift_card = Spree::GiftCard.find_by_code(@order.gift_code) and gift_card.order_activatable?(@order)
         fire_event('spree.checkout.gift_code_added', :gift_code => @order.gift_code)
         # NOTE: name gift card "Voucher" to make other discounts invalid on
