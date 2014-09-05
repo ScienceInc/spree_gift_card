00711d3510d2c4984252cf77850cfa33b0c4f9cb
Bryce Frye <bcfrye19@gmail.com>
2014-09-04 18:18:12 -0700

    fix for users accidentally adding whitespace to their codes

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -4,7 +4,9 @@ Spree::StoreController.class_eval do
 
     def apply_gift_code
       return true if @order.gift_code.blank?
+      #fixes for leading zeros chopped by Excel and users accidentally adding whitespace to codes
       @order.gift_code = "0" + @order.gift_code if @order.gift_code.length == 15
+      @order.gift_code = @order.gift_code.gsub(/\s+/, '')
       if gift_card = Spree::GiftCard.find_by_code(@order.gift_code) and gift_card.order_activatable?(@order)
         fire_event('spree.checkout.gift_code_added', :gift_code => @order.gift_code)
         # NOTE: name gift card "Voucher" to make other discounts invalid on
